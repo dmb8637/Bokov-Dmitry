3387bff05f73593db0bcf0c1d5bec50b52f7153d
dmb8637 <44606837+dmb8637@users.noreply.github.com>
2020-02-07 10:37:56 +0300

    Update app.php

--- app.php
+++ app.php
@@ -1,18 +1,12 @@
 <?php 
 
-$email = trim($_POST['email']);
-$phone = trim($_POST['phone']);
-$name = trim($_POST['name']);
 $comment = trim($_POST['comment'])
 $dt = date('Y-m-d H:i:s');
 
-if ($email == '' || $phone == '' || $name == '') {
+if ($comment == '') {
  echo "Заполните все поля";
 }
-elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
- echo "Введите корректный адрес эл. почты";
-}
 else {
- file_put_contents('apps.txt', '$dt $email $phone $name $comment \n', FILE_APPEND);
+ file_put_contents('apps.txt', '$dt $comment \n', FILE_APPEND);
  echo '1';
 }
